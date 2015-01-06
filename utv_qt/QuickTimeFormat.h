4fe607e72721bb00f6ada5429166bfac47149a04
Yuta NAKAI <nak5124@live.jp>
2015-01-07 01:55:16 +0900

    Remove unneeded files.

--- utv_qt/QuickTimeFormat.h
+++
@@ -1,7 +0,0 @@
-/* •¶šƒR[ƒh‚Í‚r‚i‚h‚r ‰üsƒR[ƒh‚Í‚b‚q‚k‚e */
-/* $Id: QuickTimeFormat.h 1203 2014-12-27 06:05:50Z umezawa $ */
-
-#pragma once
-
-int UtVideoFormatToQuickTimeFormat(OSType *pixelFormat, utvf_t utvf);
-int QuickTimeFormatToUtVideoFormat(utvf_t *utvf, OSType pixelFormat);
